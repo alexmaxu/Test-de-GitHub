5a0aeedd13a5ff8187bad17549188288c9d5dfab
alex <alexmaxu12@gmail.com>
2024-03-24 23:25:07 +0100

    Initial Commit

---
+++ Test de GitHub/Test_de_GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_de_GitHubApp.swift
+//  Test de GitHub
+//
+//  Created by Alex  on 24/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Test_de_GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
